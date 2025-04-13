9c2b1ff8c9c4bc495fe4e577268d56bde74c192b
sajidqamer <www.sajidqamer@gmail.com>
2025-04-14 03:17:49 +0500

    Initial Commit

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://msorcpgdtrseipkedumc.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zb3JjcGdkdHJzZWlwa2VkdW1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0MDk1MzYsImV4cCI6MjA1OTk4NTUzNn0.AU1FbBTnbsRJpxWju3PiN_dyouyv6cS1z4jzMQZJmfU';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
